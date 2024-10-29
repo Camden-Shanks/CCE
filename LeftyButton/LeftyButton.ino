fd4086444722c0691087673bc9fbd687146a5228
Camden-Shanks <shankscl@vcu.edu>
2024-10-29 18:32:37 -0400

    Add Code

---
+++ LeftyButton/LeftyButton.ino
@@ -0,0 +1,21 @@
+#include <Adafruit_CircuitPlayground.h>
+
+int value;
+int freq; 
+bool buttonPressed;
+void setup() {
+  // put your setup code here, to run once:
+CircuitPlayground.begin();
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+// buttonPressed = CircuitPlayground.leftButton();
+value = analogRead(A3);
+// delay(1000);
+Serial.println(value);
+
+freq=map(value, 0, 1023, 100, 1000);
+// CircuitPlayground.playTone(freq, 1000);
+}
