587f3437043d723da3ef3f7e19c78772606a1689
Camden-Shanks <shankscl@vcu.edu>
2024-10-31 18:05:58 -0400

    Adding NeoLights

--- LeftyButton/LeftyButton.ino
+++ LeftyButton/LeftyButton.ino
@@ -1,21 +1,44 @@
 #include <Adafruit_CircuitPlayground.h>
 
-int value;
-int freq; 
-bool buttonPressed;
+bool leftButtonPressed;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 CircuitPlayground.begin();
-Serial.begin(9600);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-// buttonPressed = CircuitPlayground.leftButton();
-value = analogRead(A3);
-// delay(1000);
-Serial.println(value);
+leftButtonPressed = CircuitPlayground.leftButton();
 
-freq=map(value, 0, 1023, 100, 1000);
-// CircuitPlayground.playTone(freq, 1000);
+Serial.print("Left Button:");
+if (leftButtonPressed){ 
+Serial.print("DOWN");
+} else { 
+  Serial.print("UP");
+}
+
+CircuitPlayground.setPixelColor(0, 64, 224, 208);
+CircuitPlayground.setPixelColor(1, 253, 200, 3);
+CircuitPlayground.setPixelColor(2, 64, 224, 208);
+CircuitPlayground.setPixelColor(3, 253, 200, 3);
+CircuitPlayground.setPixelColor(4, 64, 224, 208);
+CircuitPlayground.setPixelColor(5, 253, 200, 3);
+
+CircuitPlayground.setPixelColor(6, 64, 224, 208);
+CircuitPlayground.setPixelColor(7, 253, 200, 3);
+CircuitPlayground.setPixelColor(8, 64, 224, 208);
+CircuitPlayground.setPixelColor(9, 253, 200, 3);
+
+delay(2000);
+
+CircuitPlayground.clearPixels();
+
+
+// delay(1000);
+Serial.println();
+
+delay(2000);
 }
