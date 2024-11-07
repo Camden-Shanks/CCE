1ffda5c9fbb24293f263034e6138172d5e2eab21
Camden-Shanks <shankscl@vcu.edu>
2024-11-06 22:02:09 -0500

    Slide Switch & Sound Sensor

--- LeftyButton/LeftyButton.ino
+++ LeftyButton/LeftyButton.ino
@@ -1,7 +1,7 @@
 #include <Adafruit_CircuitPlayground.h>
 
 bool slideSwitch;
-bool switchOff; 
+float soundLevel; 
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,15 +12,32 @@ CircuitPlayground.begin();
 
 void loop() {
   // put your main code here, to run repeatedly:
+soundLevel = CircuitPlayground.mic.soundPressureLevel(10);
+//insert Sound Pressure level
+Serial.print("Sound Sensor SPL:");
+Serial.println(soundLevel);
 
-  //whenever left button is pressed the lights will turn on
-slideSwitch = CircuitPlayground.slideSwitch();
+//When sound sensor activates, Purple LEDs will react to sound causing intense flickering
+if (soundLevel > 50) { 
+for (int i = 0; i < 10; i++) { 
+  CircuitPlayground.setPixelColor(i, 121, 3, 253);
+}
+} else {
 CircuitPlayground.clearPixels();
+}
 
-delay(200);
+//whenever left button is pressed the lights will turn on
+slideSwitch = CircuitPlayground.slideSwitch();
+
+Serial.print("Slide Switch: ");
+  if (slideSwitch) {
+    Serial.print("+"); //Slide switch is on
+  } else {
+    Serial.print("-"); //Slide switch is off to 'sound mode'
+  }
+
 if (slideSwitch) {
-
-//Set color of every light
+//Set color of every light, mainly blue and yellow to symbolize calm silence
 CircuitPlayground.setPixelColor(0, 64, 224, 208);
 CircuitPlayground.setPixelColor(1, 253, 200, 3);
 CircuitPlayground.setPixelColor(2, 64, 224, 208);
@@ -32,21 +49,10 @@ CircuitPlayground.setPixelColor(6, 64, 224, 208);
 CircuitPlayground.setPixelColor(7, 253, 200, 3);
 CircuitPlayground.setPixelColor(8, 64, 224, 208);
 CircuitPlayground.setPixelColor(9, 253, 200, 3);
-
 } else { 
-
-CircuitPlayground.clearPixels();
+//when the switch is turned, all LEDs turn into the colors above
+  CircuitPlayground.clearPixels();
 }
-
-Serial.print("Slide Switch: ");
-if (slideSwitch){ 
-Serial.print("+");
-} else { 
-  Serial.print("-");
-}
-
-Serial.println();
-
-delay(2000);
+delay(100);
 
 }
